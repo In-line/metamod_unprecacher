e7ffa47114987ab4e8e784a553f933197e1abffe
LeninChan <767138@mail.ru>
2016-05-19 18:33:15 +0300

    Update sdk_util_custom.cpp

--- sdk_util_custom.cpp
+++ sdk_util_custom.cpp
@@ -42,67 +42,67 @@
 char g_szLogString[1024];
 void UTIL_LogToFile(char* szFileName, const char *fmt, ...)
 {
-    va_list			argptr;
-    va_start ( argptr, fmt );
-    vsnprintf ( g_szLogString, sizeof(g_szLogString), fmt, argptr );
-    va_end   ( argptr );
+	va_list		argptr;
+	va_start ( argptr, fmt );
+	vsnprintf ( g_szLogString, sizeof(g_szLogString), fmt, argptr );
+	va_end   ( argptr );
 
-    char* szFilePath = new char[strlen(GlobalVariables::g_szDLLDirPath) + 12 + 30 + 1];
-    char* szLogFile = get_timestring("_%Y%m%d.log");
-    sprintf(szFilePath, "%slogs/%s%s", (GlobalVariables::g_szDLLDirPath), szFileName, szLogFile);
-    FILE* hFile = fopen(szFilePath, "a+");
-    delete[] szFilePath;
-    delete[] szLogFile;
-    if(hFile == NULL)
-    {
-      char szError[256];
-      sprintf(szError, "Error fopen: %s\n", strerror(errno));
-      SERVER_PRINT(szError);
-      clearerr(hFile);
-      return;
-    }
+	char* szFilePath = new char[strlen(GlobalVariables::g_szDLLDirPath) + 12 + 30 + 1];
+	char* szLogFile = get_timestring("_%Y%m%d.log");
+	sprintf(szFilePath, "%slogs/%s%s", (GlobalVariables::g_szDLLDirPath), szFileName, szLogFile);
+	FILE* hFile = fopen(szFilePath, "a+");
+	delete[] szFilePath;
+	delete[] szLogFile;
+	if(hFile == NULL)
+	{
+		char szError[256];
+		sprintf(szError, "Error fopen: %s\n", strerror(errno));
+		SERVER_PRINT(szError);
+		clearerr(hFile);
+		return;
+	}
     
-    fprintf(hFile, g_szLogString);
-    if(ferror(hFile))
-    {
-      char szError[256];
-      sprintf(szError, "Error fprintf: %s\n", strerror(errno));
-      SERVER_PRINT(szError);
-      clearerr(hFile);
-      return;
-    }
-    fclose(hFile);
+	fprintf(hFile, g_szLogString);
+	if(ferror(hFile))
+	{
+		char szError[256];
+		sprintf(szError, "Error fprintf: %s\n", strerror(errno));
+		SERVER_PRINT(szError);
+		clearerr(hFile);
+		return;
+	}
+	fclose(hFile);
 
 }
 
 char* UTIL_GetLog(const char *fmt, ...)
 {
-    va_list			argptr;
-    static char		temp[1024];
-
-    va_start ( argptr, fmt );
-    vsnprintf ( temp, sizeof(temp), fmt, argptr );
-    va_end   ( argptr );
-
-    char* szDateTime = get_timestring("%d\\%m\\%Y %X");
-    sprintf(g_szLogString,"[Metamod Ultimate Unprecacher] [%s] : %s\n",szDateTime,temp);
-    delete[] szDateTime;
-
-    return str_copy(g_szLogString);
-}
+	va_list			argptr;
+	static char		temp[1024];
+	
+	va_start ( argptr, fmt );
+	vsnprintf ( temp, sizeof(temp), fmt, argptr );
+	va_end	( argptr );
+	
+	char* szDateTime = get_timestring("%d\\%m\\%Y %X");
+	sprintf(g_szLogString,"[Metamod Ultimate Unprecacher] [%s] : %s\n",szDateTime,temp);
+	delete[] szDateTime;
+	
+	return str_copy(g_szLogString);
+	}
 
 void UTIL_LogError(const char *fmt, ...)
 {
-    va_list			argptr;
-
-    va_start ( argptr, fmt );
-    vsnprintf ( g_szLogString, sizeof(g_szLogString), fmt, argptr );
-    va_end   ( argptr );
-
-    char*  szStr = UTIL_GetLog(g_szLogString);
-    strcpy(g_szLogString, szStr);
-    SERVER_PRINT(szStr);
-    UTIL_LogToFile("error", g_szLogString);
-    ALERT( at_error, szStr );
-    delete[] szStr;
+	va_list			argptr;
+	
+	va_start ( argptr, fmt );
+	vsnprintf ( g_szLogString, sizeof(g_szLogString), fmt, argptr );
+	va_end   ( argptr );
+	
+	char*  szStr = UTIL_GetLog(g_szLogString);
+	strcpy(g_szLogString, szStr);
+	SERVER_PRINT(szStr);
+	UTIL_LogToFile("error", g_szLogString);
+	ALERT( at_error, szStr );
+	delete[] szStr;
 }
