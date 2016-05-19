b879c94bb755e11b83a61be6e22e383e002cd36c
LeninChan <767138@mail.ru>
2016-05-19 18:53:22 +0300

    Update engine_api.cpp

--- engine_api.cpp
+++ engine_api.cpp
@@ -39,315 +39,315 @@
 
 void pfnSetModel(edict_t *eEnt, const char* szModel)
 {
-    unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
-    switch(szModel[0])
-    {
-        case '*':
-        {
-            RETURN_META(MRES_IGNORED);
-            break;
-        }
-        case 'm':
-        {
-            szModel+=7;
-            if(ulList->modelExists(szModel))
-            {
-                RETURN_META(MRES_SUPERCEDE);
-            }
-            break;
-        }
-        case 's':
-        {
-            szModel+=8;
-            if(ulList->spriteExists(szModel))
-            {
-                RETURN_META(MRES_SUPERCEDE);
-            }
-            break;
-        }
-    }
-   RETURN_META(MRES_IGNORED);
+	unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
+	switch(szModel[0])
+	{
+		case '*':
+		{
+			RETURN_META(MRES_IGNORED);
+			break;
+		}
+		case 'm':
+		{
+			szModel+=7;
+			if(ulList->modelExists(szModel))
+			{
+				RETURN_META(MRES_SUPERCEDE);
+			}
+			break;
+		}
+		case 's':
+		{
+			szModel+=8;
+			if(ulList->spriteExists(szModel))
+			{
+				RETURN_META(MRES_SUPERCEDE);
+			}
+			break;
+		}
+	}
+	RETURN_META(MRES_IGNORED);
 }
 
 int pfnPrecacheSound(const char* szSound)
 {
-    unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
-    if(ulList->soundExists(szSound))
-    {
-        RETURN_META_VALUE(MRES_SUPERCEDE, FALSE);
-    }
-    RETURN_META_VALUE(MRES_IGNORED, FALSE);
+	unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
+	if(ulList->soundExists(szSound))
+	{
+		RETURN_META_VALUE(MRES_SUPERCEDE, FALSE);
+	}
+	RETURN_META_VALUE(MRES_IGNORED, FALSE);
 }
 
 int pfnPrecacheModel(const char* szModel)
 {
-    unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
-    switch(szModel[0])
-    {
-        case 'm':
-        {
-            szModel+=7;
-            if(ulList->modelExists(szModel))
-            {
-                RETURN_META_VALUE(MRES_SUPERCEDE, FALSE);
-            }
-            break;
-        }
-        case 's':
-        {
-            szModel+=8;
-            if(ulList->spriteExists(szModel))
-            {
-                RETURN_META_VALUE(MRES_SUPERCEDE, FALSE);
-            }
-            break;
-        }
-    }
-    RETURN_META_VALUE(MRES_IGNORED, FALSE);
+	unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
+	switch(szModel[0])
+	{
+		case 'm':
+		{
+			szModel+=7;
+			if(ulList->modelExists(szModel))
+			{
+				RETURN_META_VALUE(MRES_SUPERCEDE, FALSE);
+			}
+			break;
+		}
+		case 's':
+		{
+			szModel+=8;
+			if(ulList->spriteExists(szModel))
+			{
+				RETURN_META_VALUE(MRES_SUPERCEDE, FALSE);
+			}
+			break;
+		}
+	}
+	RETURN_META_VALUE(MRES_IGNORED, FALSE);
 }
 
 
 void pfnEmitSound(edict_t *iEnt, int iChannel, const char *szSample, /*int*/float fVolume, float fAttenuation, int fFlags, int iPitch)
 {
-    unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
-    if(ulList->soundExists(szSample))
-    {
-        RETURN_META(MRES_SUPERCEDE);
-    }
-    RETURN_META(MRES_IGNORED);
+	unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
+	if(ulList->soundExists(szSample))
+	{
+		RETURN_META(MRES_SUPERCEDE);
+	}
+	RETURN_META(MRES_IGNORED);
 }
 void pfnEmitAmbientSound(edict_t *iEnt, float *fPos, const char *szSample, float fVol, float fAttenuation, int fFlags, int iPitch)
 {
-    unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
-    if(ulList->soundExists(szSample))
-    {
-        RETURN_META(MRES_SUPERCEDE);
-    }
-    RETURN_META(MRES_IGNORED);
+	unprecache_list* ulList = GlobalVariables::g_ulUnprecacheList;
+	if(ulList->soundExists(szSample))
+	{
+		RETURN_META(MRES_SUPERCEDE);
+	}
+	RETURN_META(MRES_IGNORED);
 }
 int pfnModelIndex(const char *szModel)
 {
-    if(szModel[0]=='m')
-    {
-        szModel+=7;
-        if(GlobalVariables::g_ulUnprecacheList->modelExists(szModel))
-        {
-            RETURN_META_VALUE(MRES_SUPERCEDE, FALSE);
-        }
-    }
-    RETURN_META_VALUE(MRES_IGNORED, FALSE);
+	if(szModel[0]=='m')
+	{
+		szModel+=7;
+		if(GlobalVariables::g_ulUnprecacheList->modelExists(szModel))
+		{
+			RETURN_META_VALUE(MRES_SUPERCEDE, FALSE);
+		}
+	}
+	RETURN_META_VALUE(MRES_IGNORED, FALSE);
 }
 
 enginefuncs_t meta_engfuncs =
 {
-    pfnPrecacheModel,						// pfnPrecacheModel()
-    pfnPrecacheSound,						// pfnPrecacheSound()
-    pfnSetModel,						// pfnSetModel()
-    pfnModelIndex,						// pfnModelIndex()
-    NULL,						// pfnModelFrames()
+	pfnPrecacheModel,			// pfnPrecacheModel()
+	pfnPrecacheSound,			// pfnPrecacheSound()
+	pfnSetModel,				// pfnSetModel()
+	pfnModelIndex,				// pfnModelIndex()
+	NULL,						// pfnModelFrames()
 
-    NULL,						// pfnSetSize()
-    NULL,						// pfnChangeLevel()
-    NULL,						// pfnGetSpawnParms()
-    NULL,						// pfnSaveSpawnParms()
+	NULL,						// pfnSetSize()
+	NULL,						// pfnChangeLevel()
+	NULL,						// pfnGetSpawnParms()
+	NULL,						// pfnSaveSpawnParms()
 
-    NULL,						// pfnVecToYaw()
-    NULL,						// pfnVecToAngles()
-    NULL,						// pfnMoveToOrigin()
-    NULL,						// pfnChangeYaw()
-    NULL,						// pfnChangePitch()
+	NULL,						// pfnVecToYaw()
+	NULL,						// pfnVecToAngles()
+	NULL,						// pfnMoveToOrigin()
+	NULL,						// pfnChangeYaw()
+	NULL,						// pfnChangePitch()
 
-    NULL,						// pfnFindEntityByString()
-    NULL,						// pfnGetEntityIllum()
-    NULL,						// pfnFindEntityInSphere()
-    NULL,						// pfnFindClientInPVS()
-    NULL,						// pfnEntitiesInPVS()
+	NULL,						// pfnFindEntityByString()
+	NULL,						// pfnGetEntityIllum()
+	NULL,						// pfnFindEntityInSphere()
+	NULL,						// pfnFindClientInPVS()
+	NULL,						// pfnEntitiesInPVS()
 
-    NULL,						// pfnMakeVectors()
-    NULL,						// pfnAngleVectors()
+	NULL,						// pfnMakeVectors()
+	NULL,						// pfnAngleVectors()
 
-    NULL,						// pfnCreateEntity()
-    NULL,						// pfnRemoveEntity()
-    NULL,						// pfnCreateNamedEntity()
+	NULL,						// pfnCreateEntity()
+	NULL,						// pfnRemoveEntity()
+	NULL,						// pfnCreateNamedEntity()
 
-    NULL,						// pfnMakeStatic()
-    NULL,						// pfnEntIsOnFloor()
-    NULL,						// pfnDropToFloor()
+	NULL,						// pfnMakeStatic()
+	NULL,						// pfnEntIsOnFloor()
+	NULL,						// pfnDropToFloor()
 
-    NULL,						// pfnWalkMove()
-    NULL,						// pfnSetOrigin()
+	NULL,						// pfnWalkMove()
+	NULL,						// pfnSetOrigin()
 
-    pfnEmitSound,						// pfnEmitSound()
-    pfnEmitAmbientSound,						// pfnEmitAmbientSound()
+	pfnEmitSound,				// pfnEmitSound()
+	pfnEmitAmbientSound,		// pfnEmitAmbientSound()
 
-    NULL,						// pfnTraceLine()
-    NULL,						// pfnTraceToss()
-    NULL,						// pfnTraceMonsterHull()
-    NULL,						// pfnTraceHull()
-    NULL,						// pfnTraceModel()
-    NULL,						// pfnTraceTexture()
-    NULL,						// pfnTraceSphere()
-    NULL,						// pfnGetAimVector()
+	NULL,						// pfnTraceLine()
+	NULL,						// pfnTraceToss()
+	NULL,						// pfnTraceMonsterHull()
+	NULL,						// pfnTraceHull()
+	NULL,						// pfnTraceModel()
+	NULL,						// pfnTraceTexture()
+	NULL,						// pfnTraceSphere()
+	NULL,						// pfnGetAimVector()
 
-    NULL,						// pfnServerCommand()
-    NULL,						// pfnServerExecute()
-    NULL,						// pfnClientCommand()
+	NULL,						// pfnServerCommand()
+	NULL,						// pfnServerExecute()
+	NULL,						// pfnClientCommand()
 
-    NULL,						// pfnParticleEffect()
-    NULL,						// pfnLightStyle()
-    NULL,						// pfnDecalIndex()
-    NULL,						// pfnPointContents()
+	NULL,						// pfnParticleEffect()
+	NULL,						// pfnLightStyle()
+	NULL,						// pfnDecalIndex()
+	NULL,						// pfnPointContents()
 
-    NULL,						// pfnMessageBegin()
-    NULL,						// pfnMessageEnd()
+	NULL,						// pfnMessageBegin()
+	NULL,						// pfnMessageEnd()
 
-    NULL,						// pfnWriteByte()
-    NULL,						// pfnWriteChar()
-    NULL,						// pfnWriteShort()
-    NULL,						// pfnWriteLong()
-    NULL,						// pfnWriteAngle()
-    NULL,						// pfnWriteCoord()
-    NULL,						// pfnWriteString()
-    NULL,						// pfnWriteEntity()
+	NULL,						// pfnWriteByte()
+	NULL,						// pfnWriteChar()
+	NULL,						// pfnWriteShort()
+	NULL,						// pfnWriteLong()
+	NULL,						// pfnWriteAngle()
+	NULL,						// pfnWriteCoord()
+	NULL,						// pfnWriteString()
+	NULL,						// pfnWriteEntity()
 
-    NULL,						// pfnCVarRegister()
-    NULL,						// pfnCVarGetFloat()
-    NULL,						// pfnCVarGetString()
-    NULL,						// pfnCVarSetFloat()
-    NULL,						// pfnCVarSetString()
+	NULL,						// pfnCVarRegister()
+	NULL,						// pfnCVarGetFloat()
+	NULL,						// pfnCVarGetString()
+	NULL,						// pfnCVarSetFloat()
+	NULL,						// pfnCVarSetString()
 
-    NULL,						// pfnAlertMessage()
-    NULL,						// pfnEngineFprintf()
+	NULL,						// pfnAlertMessage()
+	NULL,						// pfnEngineFprintf()
 
-    NULL,						// pfnPvAllocEntPrivateData()
-    NULL,						// pfnPvEntPrivateData()
-    NULL,						// pfnFreeEntPrivateData()
+	NULL,						// pfnPvAllocEntPrivateData()
+	NULL,						// pfnPvEntPrivateData()
+	NULL,						// pfnFreeEntPrivateData()
 
-    NULL,						// pfnSzFromIndex()
-    NULL,						// pfnAllocString()
+	NULL,						// pfnSzFromIndex()
+	NULL,						// pfnAllocString()
 
-    NULL, 						// pfnGetVarsOfEnt()
-    NULL,						// pfnPEntityOfEntOffset()
-    NULL,						// pfnEntOffsetOfPEntity()
-    NULL,						// pfnIndexOfEdict()
-    NULL,						// pfnPEntityOfEntIndex()
-    NULL,						// pfnFindEntityByVars()
-    NULL,						// pfnGetModelPtr()
+	NULL, 						// pfnGetVarsOfEnt()
+	NULL,						// pfnPEntityOfEntOffset()
+	NULL,						// pfnEntOffsetOfPEntity()
+	NULL,						// pfnIndexOfEdict()
+	NULL,						// pfnPEntityOfEntIndex()
+	NULL,						// pfnFindEntityByVars()
+	NULL,						// pfnGetModelPtr()
 
-    NULL,						// pfnRegUserMsg()
+	NULL,						// pfnRegUserMsg()
 
-    NULL,						// pfnAnimationAutomove()
-    NULL,						// pfnGetBonePosition()
+	NULL,						// pfnAnimationAutomove()
+	NULL,						// pfnGetBonePosition()
 
-    NULL,						// pfnFunctionFromName()
-    NULL,						// pfnNameForFunction()
+	NULL,						// pfnFunctionFromName()
+	NULL,						// pfnNameForFunction()
 
-    NULL,						// pfnClientPrintf()
-    NULL,						// pfnServerPrint()
+	NULL,						// pfnClientPrintf()
+	NULL,						// pfnServerPrint()
 
-    NULL,						// pfnCmd_Args()
-    NULL,						// pfnCmd_Argv()
-    NULL,						// pfnCmd_Argc()
+	NULL,						// pfnCmd_Args()
+	NULL,						// pfnCmd_Argv()
+	NULL,						// pfnCmd_Argc()
 
-    NULL,						// pfnGetAttachment()
+	NULL,						// pfnGetAttachment()
 
-    NULL,						// pfnCRC32_Init()
-    NULL,						// pfnCRC32_ProcessBuffer()
-    NULL,						// pfnCRC32_ProcessByte()
-    NULL,						// pfnCRC32_Final()
+	NULL,						// pfnCRC32_Init()
+	NULL,						// pfnCRC32_ProcessBuffer()
+	NULL,						// pfnCRC32_ProcessByte()
+	NULL,						// pfnCRC32_Final()
 
-    NULL,						// pfnRandomLong()
-    NULL,						// pfnRandomFloat()
+	NULL,						// pfnRandomLong()
+	NULL,						// pfnRandomFloat()
 
-    NULL,						// pfnSetView()
-    NULL,						// pfnTime()
-    NULL,						// pfnCrosshairAngle()
+	NULL,						// pfnSetView()
+	NULL,						// pfnTime()
+	NULL,						// pfnCrosshairAngle()
 
-    NULL,						// pfnLoadFileForMe()
-    NULL,						// pfnFreeFile()
+	NULL,						// pfnLoadFileForMe()
+	NULL,						// pfnFreeFile()
 
-    NULL,						// pfnEndSection()
-    NULL,						// pfnCompareFileTime()
-    NULL,						// pfnGetGameDir()
-    NULL,						// pfnCvar_RegisterVariable()
-    NULL,						// pfnFadeClientVolume()
-    NULL,						// pfnSetClientMaxspeed()
-    NULL,						// pfnCreateFakeClient()
-    NULL,						// pfnRunPlayerMove()
-    NULL,						// pfnNumberOfEntities()
+	NULL,						// pfnEndSection()
+	NULL,						// pfnCompareFileTime()
+	NULL,						// pfnGetGameDir()
+	NULL,						// pfnCvar_RegisterVariable()
+	NULL,						// pfnFadeClientVolume()
+	NULL,						// pfnSetClientMaxspeed()
+	NULL,						// pfnCreateFakeClient()
+	NULL,						// pfnRunPlayerMove()
+	NULL,						// pfnNumberOfEntities()
 
-    NULL,						// pfnGetInfoKeyBuffer()
-    NULL,						// pfnInfoKeyValue()
-    NULL,						// pfnSetKeyValue()
-    NULL,						// pfnSetClientKeyValue()
+	NULL,						// pfnGetInfoKeyBuffer()
+	NULL,						// pfnInfoKeyValue()
+	NULL,						// pfnSetKeyValue()
+	NULL,						// pfnSetClientKeyValue()
 
-    NULL,						// pfnIsMapValid()
-    NULL,						// pfnStaticDecal()
-    NULL,						// pfnPrecacheGeneric()
-    NULL, 						// pfnGetPlayerUserId()
-    NULL,						// pfnBuildSoundMsg()
-    NULL,						// pfnIsDedicatedServer()
-    NULL,						// pfnCVarGetPointer()
-    NULL,						// pfnGetPlayerWONId()
+	NULL,						// pfnIsMapValid()
+	NULL,						// pfnStaticDecal()
+	NULL,						// pfnPrecacheGeneric()
+	NULL, 						// pfnGetPlayerUserId()
+	NULL,						// pfnBuildSoundMsg()
+	NULL,						// pfnIsDedicatedServer()
+	NULL,						// pfnCVarGetPointer()
+	NULL,						// pfnGetPlayerWONId()
 
-    NULL,						// pfnInfo_RemoveKey()
-    NULL,						// pfnGetPhysicsKeyValue()
-    NULL,						// pfnSetPhysicsKeyValue()
-    NULL,						// pfnGetPhysicsInfoString()
-    NULL,						// pfnPrecacheEvent()
-    NULL,						// pfnPlaybackEvent()
+	NULL,						// pfnInfo_RemoveKey()
+	NULL,						// pfnGetPhysicsKeyValue()
+	NULL,						// pfnSetPhysicsKeyValue()
+	NULL,						// pfnGetPhysicsInfoString()
+	NULL,						// pfnPrecacheEvent()
+	NULL,						// pfnPlaybackEvent()
 
-    NULL,						// pfnSetFatPVS()
-    NULL,						// pfnSetFatPAS()
+	NULL,						// pfnSetFatPVS()
+	NULL,						// pfnSetFatPAS()
 
-    NULL,						// pfnCheckVisibility()
+	NULL,						// pfnCheckVisibility()
 
-    NULL,						// pfnDeltaSetField()
-    NULL,						// pfnDeltaUnsetField()
-    NULL,						// pfnDeltaAddEncoder()
-    NULL,						// pfnGetCurrentPlayer()
-    NULL,						// pfnCanSkipPlayer()
-    NULL,						// pfnDeltaFindField()
-    NULL,						// pfnDeltaSetFieldByIndex()
-    NULL,						// pfnDeltaUnsetFieldByIndex()
+	NULL,						// pfnDeltaSetField()
+	NULL,						// pfnDeltaUnsetField()
+	NULL,						// pfnDeltaAddEncoder()
+	NULL,						// pfnGetCurrentPlayer()
+	NULL,						// pfnCanSkipPlayer()
+	NULL,						// pfnDeltaFindField()
+	NULL,						// pfnDeltaSetFieldByIndex()
+	NULL,						// pfnDeltaUnsetFieldByIndex()
 
-    NULL,						// pfnSetGroupMask()
+	NULL,						// pfnSetGroupMask()
 
-    NULL,						// pfnCreateInstancedBaseline()
-    NULL,						// pfnCvar_DirectSet()
+	NULL,						// pfnCreateInstancedBaseline()
+	NULL,						// pfnCvar_DirectSet()
 
-    NULL,						// pfnForceUnmodified()
+	NULL,						// pfnForceUnmodified()
 
-    NULL,						// pfnGetPlayerStats()
+	NULL,						// pfnGetPlayerStats()
 
-    NULL,						// pfnAddServerCommand()
+	NULL,						// pfnAddServerCommand()
 
 	// Added in SDK 2.2:
-    NULL,						// pfnVoice_GetClientListening()
-    NULL,						// pfnVoice_SetClientListening()
+	NULL,						// pfnVoice_GetClientListening()
+	NULL,						// pfnVoice_SetClientListening()
 
 	// Added for HL 1109 (no SDK update):
-    NULL,						// pfnGetPlayerAuthId()
+	NULL,						// pfnGetPlayerAuthId()
 
 	// Added 2003-11-10 (no SDK update):
-    NULL,						// pfnSequenceGet()
-    NULL,						// pfnSequencePickSentence()
-    NULL,						// pfnGetFileSize()
-    NULL,						// pfnGetApproxWavePlayLen()
-    NULL,						// pfnIsCareerMatch()
-    NULL,						// pfnGetLocalizedStringLength()
-    NULL,						// pfnRegisterTutorMessageShown()
-    NULL,						// pfnGetTimesTutorMessageShown()
-    NULL,						// pfnProcessTutorMessageDecayBuffer()
-    NULL,						// pfnConstructTutorMessageDecayBuffer()
-    NULL,						// pfnResetTutorMessageDecayData()
+	NULL,						// pfnSequenceGet()
+	NULL,						// pfnSequencePickSentence()
+	NULL,						// pfnGetFileSize()
+	NULL,						// pfnGetApproxWavePlayLen()
+	NULL,						// pfnIsCareerMatch()
+	NULL,						// pfnGetLocalizedStringLength()
+	NULL,						// pfnRegisterTutorMessageShown()
+	NULL,						// pfnGetTimesTutorMessageShown()
+	NULL,						// pfnProcessTutorMessageDecayBuffer()
+	NULL,						// pfnConstructTutorMessageDecayBuffer()
+	NULL,						// pfnResetTutorMessageDecayData()
 
 	// Added Added 2005-08-11 (no SDK update)
-    NULL,						// pfnQueryClientCvarValue()
+	NULL,						// pfnQueryClientCvarValue()
 	// Added Added 2005-11-22 (no SDK update)
-    NULL,						// pfnQueryClientCvarValue2()
+	NULL,						// pfnQueryClientCvarValue2()
 	// Added 2009-06-17 (no SDK update)
-    NULL,						// pfnCheckParm()
+	NULL,						// pfnCheckParm()
 };
 
 
@@ -355,14 +355,14 @@ C_DLLEXPORT int GetEngineFunctions(enginefuncs_t *pengfuncsFromEngine,
 		int *interfaceVersion) 
 {
 	if(!pengfuncsFromEngine) {
-        UTIL_LogError("[Error] GetEngineFunctions called with null pengfuncsFromEngine");
+		UTIL_LogError("[Error] GetEngineFunctions called with null pengfuncsFromEngine");
 		return(FALSE);
 	}
 	else if(*interfaceVersion != ENGINE_INTERFACE_VERSION) {
-        UTIL_LogError("[Error] GetEngineFunctions version mismatch; requested=%d ours=%d", *interfaceVersion, ENGINE_INTERFACE_VERSION);
-        *interfaceVersion = ENGINE_INTERFACE_VERSION;
+		UTIL_LogError("[Error] GetEngineFunctions version mismatch; requested=%d ours=%d", *interfaceVersion, ENGINE_INTERFACE_VERSION);
+		*interfaceVersion = ENGINE_INTERFACE_VERSION;
 		return(FALSE);
 	}
-    memcpy(pengfuncsFromEngine, &meta_engfuncs, sizeof(enginefuncs_t));
+	memcpy(pengfuncsFromEngine, &meta_engfuncs, sizeof(enginefuncs_t));
 	return(TRUE);
 }
