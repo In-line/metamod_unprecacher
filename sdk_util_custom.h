a4e4b7adaecee3c55340ffefe2a9056c190cc921
In-line <cplusplus256@gmail.com>
2016-05-20 18:18:53 +0400

    Add UTIL_LogToFile(...) to header

--- sdk_util_custom.h
+++ sdk_util_custom.h
@@ -2,6 +2,7 @@
 #define SDK_UTIL_CUSTOM_H
 #include <extdll.h>
 #include <meta_api.h>
+void UTIL_LogToFile(char* szFileName, const char *fmt, ...);
 void UTIL_LogError(const char *fmt, ...);
 bool CreateDirectoryIfNotExists(char *szPath);
 #include <sdk_util.h>
