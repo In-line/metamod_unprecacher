359a46ee509843ba378b5313d3aa3caa6434c529
DYXX <dyxxscript@gmail.com>
2016-05-30 14:34:44 +0400

    Refactor unprecache_list_string

--- unprecache_list_string.h
+++ unprecache_list_string.h
@@ -32,21 +32,79 @@
 
 #ifndef UNPRECACHE_LIST_STRING_H
 #define UNPRECACHE_LIST_STRING_H
-
+#include <cstring>
 
 class unprecache_list_string
 {
 private:
 	char* m_szString;
-public:
-	unprecache_list_string(char *szStr);
-	unprecache_list_string(const char *szStr);
-	//virtual ~unprecache_list_string();
-	unprecache_list_string();
-	bool operator<(const unprecache_list_string szS1) const;
+	size_t m_iStringLen;
+	inline char* CopyString(char *szStr, int iLen)
+	{
+		char* szReturn = new char[iLen + 1];
+		strcpy(szReturn, szStr);
+		return szReturn;
+	}
 
-	//operator char*() const;
-	char* c_str() const;
+public:
+	inline unprecache_list_string(char *szStr)
+	{
+		m_iStringLen = strlen(szStr);
+		m_szString = CopyString(szStr, m_iStringLen);
+	}
+
+	inline unprecache_list_string(const char *szStr)
+	{
+		m_iStringLen = strlen(szStr);
+		m_szString = CopyString((char*)szStr, m_iStringLen);
+	}
+
+	inline unprecache_list_string()
+	{
+		m_iStringLen = 0;
+		m_szString = nullptr;
+	}
+	unprecache_list_string& operator=(unprecache_list_string&&) = delete;	// Disallow moving
+
+	inline unprecache_list_string( const unprecache_list_string &obj)
+	{
+		m_szString = CopyString(obj.m_szString, obj.m_iStringLen);
+		m_iStringLen = obj.m_iStringLen;
+	}
+
+	inline ~unprecache_list_string()
+	{
+		if(m_szString!=nullptr) delete[] m_szString;
+	}
+
+	inline bool operator<(const unprecache_list_string szS2) const
+	{
+		char* szStr2 = szS2.c_str();
+		if(m_szString == nullptr)
+			return szStr2==m_szString;
+
+		register char c;
+		register char c2;
+		for(unsigned int i = 0;;++i)
+		{
+			c = m_szString[i];
+			c2 = szStr2[i];
+			if(c < c2)
+			{
+				return true;
+			}
+			else if(c > c2 || c=='\0' || c2=='\0')
+			{
+				return false;
+			}
+		}
+		return false;
+	}
+
+	inline char* c_str() const
+	{
+		return m_szString;
+	}
 };
 
 #endif // UNPRECACHE_LIST_STRING_H
